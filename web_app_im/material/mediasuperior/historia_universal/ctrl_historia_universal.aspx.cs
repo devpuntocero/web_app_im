284cfe6ee0fac6ade5adeb199ca59d25c47cabf4
intelimundo <intelimundo@HPROJA>
2018-02-22 17:14:25 -0600

    agregar  temario

---
+++ web_app_im/material/mediasuperior/historia_universal/ctrl_historia_universal.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_app_im.material.mediasuperior.historia_universal
+{
+    public partial class ctrl_historia_universal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
