ffd2a13c9ff25df28d591cf8423129839038b2b8
Axel Velazquez Merino <axelvelazquezmerino@outlook.com>
2018-02-22 16:41:36 -0600

    web_app_im

---
+++ web_app_im/material/ctrl_media_superior.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_app_im.material
+{
+    public partial class ctrl_media_superior : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void img_biologia_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("/material/mediasuperior/biologia/ctrl_biologia.aspx");
+        }
+    }
+}
