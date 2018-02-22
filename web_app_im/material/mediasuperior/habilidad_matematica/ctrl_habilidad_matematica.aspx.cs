b32da051d97946043c018fdd9405938638df9512
Intelimundo <Intelimundo@HPCHICABLANCA>
2018-02-22 17:13:58 -0600

    anexo de: español, civica, biologia,geografia,habilidad matematica

---
+++ web_app_im/material/mediasuperior/habilidad_matematica/ctrl_habilidad_matematica.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web_app_im.material.mediasuperior.habilidad_matematica
+{
+    public partial class ctrl_habilidad_matematica : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
